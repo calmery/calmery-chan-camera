1dbce1ca7b623281ff6a4d42b72bac5ffa790032
Marei Kikukawa <contact@calmery.me>
2019-11-17 19:43:11 +0900

    画像の拡大縮小、回転ができるようにする

--- src/containers/Canvas/Canvas.tsx
+++ src/containers/Canvas/Canvas.tsx
@@ -63,19 +63,36 @@ class Canvas extends React.Component<{}, ICanvasState> {
     }
 
     return (
-      <svg
-        ref={this.container}
-        width={canvas.width}
-        height={canvas.height}
-        viewBox={`0 0 ${baseLayer.width} ${baseLayer.height}`}
-        version="1.1"
-        baseProfile="full"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        onMouseMove={this.handleOnMouseMove}
-      >
-        {canvasLayers.map(this.renderCanvasLayer)}
-      </svg>
+      <React.Fragment>
+        <svg
+          ref={this.container}
+          width={canvas.width}
+          height={canvas.height}
+          viewBox={`0 0 ${baseLayer.width} ${baseLayer.height}`}
+          version="1.1"
+          baseProfile="full"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          onMouseMove={this.handleOnMouseMove}
+        >
+          {canvasLayers.map(this.renderCanvasLayer)}
+        </svg>
+
+        <input
+          type="range"
+          min="1"
+          max="5"
+          defaultValue="1"
+          onChange={this.handleOnChangeScale}
+        />
+        <input
+          type="range"
+          min="0"
+          max="359"
+          defaultValue="0"
+          onChange={this.handleOnChangeRotate}
+        />
+      </React.Fragment>
     );
   };
 
@@ -135,6 +152,61 @@ class Canvas extends React.Component<{}, ICanvasState> {
   };
 
   // Events
+
+  private handleOnChangeScale = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { selectedLayerIndex, canvasLayers } = this.state;
+
+    if (selectedLayerIndex === null) {
+      return;
+    }
+
+    const scale = parseInt(event.target.value, 10);
+    const canvasLayer = canvasLayers[selectedLayerIndex!];
+
+    canvasLayers[selectedLayerIndex!] = {
+      ...canvasLayer,
+      x:
+        canvasLayer.x +
+        (canvasLayer.width * canvasLayer.effects.scale -
+          canvasLayer.width * scale) /
+          2,
+      y:
+        canvasLayer.y +
+        (canvasLayer.height * canvasLayer.effects.scale -
+          canvasLayer.height * scale) /
+          2,
+      effects: {
+        ...canvasLayer.effects,
+        scale
+      }
+    };
+
+    this.setState({ canvasLayers });
+  };
+
+  private handleOnChangeRotate = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { selectedLayerIndex, canvasLayers } = this.state;
+
+    if (selectedLayerIndex === null) {
+      return;
+    }
+
+    const canvasLayer = canvasLayers[selectedLayerIndex!];
+
+    canvasLayers[selectedLayerIndex!] = {
+      ...canvasLayer,
+      effects: {
+        ...canvasLayer.effects,
+        rotate: parseInt(event.target.value, 10)
+      }
+    };
+
+    this.setState({ canvasLayers });
+  };
 
   private handleOnMouseMove = (
     event: React.MouseEvent<SVGSVGElement, MouseEvent>
@@ -204,7 +276,7 @@ class Canvas extends React.Component<{}, ICanvasState> {
       return;
     }
 
-    this.setState({ isDragging: false, selectedLayerIndex: null });
+    this.setState({ isDragging: false });
   };
 }
 
