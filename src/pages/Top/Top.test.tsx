98d00749da868a82ed37fc09680cb5dcf7c1fa1a
Marei Kikukawa <contact@calmery.me>
2019-11-18 02:00:59 +0900

    テストが失敗する問題を修正する

--- src/pages/Top/Top.test.tsx
+++ src/pages/Top/Top.test.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import Top from "./Top";
-import { store } from "../../modules";
 import { Provider } from "react-redux";
+import { ConnectedRouter as Router } from "connected-react-router";
+import { store, history } from "../../modules";
+import Top from "./Top";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
     <Provider store={store}>
-      <Top />
+      <Router history={history}>
+        <Top />
+      </Router>
     </Provider>,
     div
   );
