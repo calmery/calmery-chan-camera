d7d056ba564adabf5d128b3ec963e14e9730e8ef
Marei Kikukawa <contact@calmery.me>
2019-11-21 00:07:01 +0900

    ロゴを追加する

--- src/pages/Top/Top.tsx
+++ src/pages/Top/Top.tsx
@@ -1,6 +1,14 @@
 import * as React from "react";
 import Canvas from "../../containers/Canvas";
+import styles from "./Top.scss";
 
-const Top: React.FC = () => <Canvas />;
+const Top: React.FC = () => (
+  <React.Fragment>
+    <div className={styles.logo}>
+      <img src="logo.png" />
+    </div>
+    <Canvas />
+  </React.Fragment>
+);
 
 export default Top;
