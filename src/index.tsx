fb6e232dd6f1a498180511893bb27f66ddb0e647
Marei Kikukawa <contact@calmery.me>
2019-11-22 22:18:27 +0900

    Sentry を追加する

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import ReactGA from "react-ga";
+import * as Sentry from "@sentry/browser";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter as Router } from "connected-react-router";
@@ -8,6 +9,12 @@ import { store, history } from "./modules";
 import * as serviceWorker from "./serviceWorker";
 import "animate.css/animate.css";
 import Top from "./pages/Top";
+
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://69015ac1a388461b9267b07ef2f9f96d@sentry.io/1830950"
+  });
+}
 
 ReactGA.initialize("UA-153119606-1", {
   debug: process.env.NODE_ENV !== "production"
