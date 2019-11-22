8d9a564f1eb60633ffac20ac463e20ef359f9517
Marei Kikukawa <contact@calmery.me>
2019-11-22 22:05:18 +0900

    Google Analytics を追加する

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import ReactGA from "react-ga";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter as Router } from "connected-react-router";
@@ -7,6 +8,10 @@ import { store, history } from "./modules";
 import * as serviceWorker from "./serviceWorker";
 import "animate.css/animate.css";
 import Top from "./pages/Top";
+
+ReactGA.initialize("UA-153119606-1", {
+  debug: process.env.NODE_ENV !== "production"
+});
 
 serviceWorker.register();
 
--- src/pages/Top/Top.tsx
+++ src/pages/Top/Top.tsx
@@ -1,17 +1,24 @@
 import * as React from "react";
+import ReactGA from "react-ga";
 import Canvas from "../../containers/Canvas";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import styles from "./Top.scss";
 
-const Top: React.FC = () => (
-  <div className={styles.container}>
-    <Header />
-    <div className={styles.contents}>
-      <Canvas />
+class Top extends React.Component {
+  public componentDidMount = () => {
+    ReactGA.pageview(window.location.pathname);
+  };
+
+  public render = () => (
+    <div className={styles.container}>
+      <Header />
+      <div className={styles.contents}>
+        <Canvas />
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-);
+  );
+}
 
 export default Top;
